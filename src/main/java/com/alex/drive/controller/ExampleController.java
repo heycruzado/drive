6733f03b046d57ada4103db055d6acc4790f8e12
Alex Cruzado <alex.cruzado07@gmail.com>
2021-06-30 22:15:11 -0500

    In the class ExampleController were added 3 methods, helloWorld ,helloWorldWithParam, createPerson

--- src/main/java/com/alex/drive/controller/ExampleController.java
+++ src/main/java/com/alex/drive/controller/ExampleController.java
@@ -1,4 +1,24 @@
 package com.alex.drive.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/v1")
 public class ExampleController {
+    @GetMapping("/hello")
+    public String helloWorld(){
+        return "Hello World";
+    }
+
+    @GetMapping("/hello/{param}")
+    public String helloWorldWithParam(@PathVariable("param") String param){
+        return "Hello world with: "+param;
+    }
+
+    @PostMapping("/hello")
+    public ResponseEntity<String> createPerson(@RequestBody String person){
+        return ResponseEntity.status(HttpStatus.OK).body("The person "+person+" was created :D");
+    }
 }
